e6ccef70afcd162547038b2590608cd3c6e6a15d
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-21 16:57:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+  
+def dictionary 
+  dictionary = {
+    "hello" => "hi",
+    "to"=> '2',
+    "two"=> '2', 
+    "too"=> '2', 
+    "for"=> '4',
+    "four"=> '4',
+    "be"=> 'b',
+    'you' => 'u',
+    "at"=> "@",
+    "and"=> "&"
+  }
+end 
+
+
+def word_substituter (tweet)
+ tweet.gsub(/\w+/) do |w|
+  dictionary.fetch(w,w)
+  end
+end 
+
+def bulk_tweet_shortener (arr_of_tweets)
+result = []
+  arr_of_tweets.collect do |tweet|
+     tweet.gsub(/\w+/) do |w|
+     dictionary.fetch(w,w)
+  end
+end
+end  
+
